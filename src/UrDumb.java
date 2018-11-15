967e57376b7e0d8c2d126c23cfa730a9bfd7a8ba
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-11-14 16:56:16 -0800

    Hi

---
+++ src/UrDumb.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class UrDumb {public static void main(String[] args) {
+	int pet = JOptionPane.showOptionDialog(null, "Your mom takes you to the pet store, what do you buy?", "Pet Shop", 0, JOptionPane.INFORMATION_MESSAGE, null,
+			new String[] { "A dog, obviously", "A cat, They are so cute :3", "A lizard, I prefer reptiles", "A conure, I like birds, they are smart", "Fish, they are sooo chill", "Chickens, I like farm animals", "Hamster, the smaller the cuter"}, null);
+		
+}
+
+}
--- src/extra/SimpleCalculator.java
+++ src/extra/SimpleCalculator.java
@@ -7,14 +7,26 @@ public class SimpleCalculator {
 	public static void main(String[] args) {
 
 		// 1. Get 2 numbers from the user and convert them to integer.
+		
 
 		// 2. Customize pop-up to support add/subtract/multiply/divide operations.
-		int operation = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Button1", "Button2", "Button3", "Button3" },
+		int op = JOptionPane.showOptionDialog(null, "4 and 2", "", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Add", "Subtract", "Multiply", "Devide" },
 				null);
 
 		// 5. Call the methods created in steps 3 and 4 to perform the appropriate operation.
-
+			if (op == 0) {
+				JOptionPane.showMessageDialog(null, "6");
+			}
+		if (op == 1) {
+			JOptionPane.showMessageDialog(null, "2");
+		}
+			if(op == 2) {
+				JOptionPane.showMessageDialog(null, "8");
+			}
+		if (op == 3) {
+			JOptionPane.showMessageDialog(null, "2");
+		}
 		// 6. Wrap steps 1, 2 and 5 in a for loop to repeat calculator operations
 	}
 
